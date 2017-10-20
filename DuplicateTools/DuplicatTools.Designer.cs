b9fc05a4f5f90b8e0a6a1a4bf711ed19d4761d5c
panhao <29347213@qq.com>
2017-10-20 16:46:35 +0800

    Add new tool

---
+++ DuplicateTools/DuplicatTools.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace DuplicateTools
+{
+    partial class DuplicatTools
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DuplicatTools));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.moveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.moveToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moveToolStripMenuItem1,
+            this.displayToolStripMenuItem,
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // moveToolStripMenuItem1
+            // 
+            this.moveToolStripMenuItem1.Name = "moveToolStripMenuItem1";
+            this.moveToolStripMenuItem1.Size = new System.Drawing.Size(45, 21);
+            this.moveToolStripMenuItem1.Text = "Path";
+            this.moveToolStripMenuItem1.Click += new System.EventHandler(this.basepathToolStripMenuItem1_Click);
+            // 
+            // displayToolStripMenuItem
+            // 
+            this.displayToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pathToolStripMenuItem,
+            this.nameToolStripMenuItem,
+            this.sizeToolStripMenuItem});
+            this.displayToolStripMenuItem.Name = "displayToolStripMenuItem";
+            this.displayToolStripMenuItem.Size = new System.Drawing.Size(62, 21);
+            this.displayToolStripMenuItem.Text = "Display";
+            // 
+            // pathToolStripMenuItem
+            // 
+            this.pathToolStripMenuItem.Name = "pathToolStripMenuItem";
+            this.pathToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pathToolStripMenuItem.Text = "Path";
+            this.pathToolStripMenuItem.Click += new System.EventHandler(this.pathToolStripMenuItem_Click);
+            // 
+            // nameToolStripMenuItem
+            // 
+            this.nameToolStripMenuItem.Name = "nameToolStripMenuItem";
+            this.nameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nameToolStripMenuItem.Text = "Name";
+            this.nameToolStripMenuItem.Click += new System.EventHandler(this.nameToolStripMenuItem_Click);
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sizeToolStripMenuItem.Text = "Size";
+            this.sizeToolStripMenuItem.Click += new System.EventHandler(this.sizeToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moveToolStripMenuItem2});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(42, 21);
+            this.fileToolStripMenuItem.Text = "Edit";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(390, 490);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(394, 70);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(390, 490);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_RowEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(0, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(784, 36);
+            this.textBox1.TabIndex = 3;
+            // 
+            // moveToolStripMenuItem2
+            // 
+            this.moveToolStripMenuItem2.Name = "moveToolStripMenuItem2";
+            this.moveToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.moveToolStripMenuItem2.Text = "Move";
+            this.moveToolStripMenuItem2.Click += new System.EventHandler(this.moveToolStripMenuItem2_Click);
+            // 
+            // DuplicatTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DuplicatTools";
+            this.Text = "DuplicatTools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem moveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem displayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveToolStripMenuItem2;
+    }
+}
+
---
+++ DuplicateTools/DuplicatTools.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuplicateTools
+{
+   
+    public partial class DuplicatTools : Form
+    {
+        public class M_File
+        {
+            public string path_ = "";
+            public string shortpath_ = "";
+            public string name_ = "";
+            public long size_ = 0;
+            public string MD5 = "";   
+            public M_File(string path, string Folder)
+            {
+                path_ = path;
+                name_ = Path.GetFileName(path);
+                FileInfo MyFileInfo = new FileInfo(path);
+                size_ = MyFileInfo.Length;
+                shortpath_ = path.Remove(0, Folder.Length);
+            }
+            public void GetMD5HashFromFile()
+            {if (MD5 != "") return;
+                try
+                {
+                    FileStream file = new FileStream(this.path_, FileMode.Open);
+                    System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                    byte[] retVal = md5.ComputeHash(file);
+                    file.Close();
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < retVal.Length; i++)
+                    {
+                        sb.Append(retVal[i].ToString("x2"));
+                    }
+                    MD5= sb.ToString();
+                }
+                catch 
+                {
+                    MD5 = "";
+                    return;
+                }
+            }
+            public bool equalto(M_File f1)
+            {
+                if (f1.size_ != this.size_) return false;
+                else
+                {
+                    this.GetMD5HashFromFile();
+                    f1.GetMD5HashFromFile();
+                    if (this.MD5 == f1.MD5) return true;
+                     return false;
+                }
+               
+            }
+        }
+        List<M_File> files = new List<M_File>();
+        List<int> dumplsit = new List<int>();
+        List<int> newlist = new List<int>();
+        public DuplicatTools()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(800, 600);
+            this.dataGridView1.Size = new Size(390, 490);
+            this.dataGridView2.Size = new Size(390, 490);
+            this.dataGridView1.Location = new Point(0, 70);
+            this.dataGridView2.Location = new Point(394, 70);
+            this.textBox1.Location = new Point(0, 26);
+            this.textBox1.Size = new Size(784, 42);
+            dataGridView1.ColumnCount = 1;
+            dataGridView2.ColumnCount = 1;
+        }
+        public string OpenFolder()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                return fbd.SelectedPath;
+            }
+            return null;
+        }
+        private int SortList(M_File a, M_File b) 
+        {
+            if (a == null&&b!=null) return -1;
+            if (b == null&&a!=null) return 1;
+            if (b == null && a == null) return 0;
+            if (a.size_ > b.size_) 
+            {
+                return 1;
+            }
+            else if (a.size_ < b.size_)
+            {
+                return -1;
+            }
+            return 0;
+        }
+        private void basepathToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string path = OpenFolder();
+            if (path == null || path == "") { return; }
+            if (Directory.Exists(path)) textBox1.Text = path;
+            List<string> path_ = new List<string>();         
+            ListAllFiles(textBox1.Text, ref path_);
+            if (path_.Count < 2) return;
+            files.Clear();
+            for (int i = 0; i < path_.Count; i++)
+            {
+                files.Add(new M_File(path_[i],path));
+            }
+            if (this.files.Count < 2) return;
+            files.Sort(SortList);
+            dumplsit = new List<int>();
+            newlist = new List<int>();
+            newlist.Add(0);
+            for (int i = 1; i < this.files.Count; i++)
+            {
+                bool sign = true;
+                for (int j = 0; j < newlist.Count; j++)
+                {
+                    if (files[i].equalto(files[newlist[j]]))
+                    {
+                        sign = false;
+                    }
+                }
+                if (sign) { newlist.Add(i); }
+                else { dumplsit.Add(i); }
+            }
+            dataGridView1.RowCount = files.Count;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = files[i].shortpath_;
+            }
+            if (dumplsit.Count < 1) return;
+            dataGridView2.RowCount = dumplsit.Count;
+            for (int i = 0; i < dumplsit.Count; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = files[dumplsit[i]].shortpath_;
+            }
+        }       
+        public static void ListAllFiles(string dir, ref List<string> output)
+        {
+            if (Directory.Exists(dir))
+            {
+                foreach (string d in Directory.GetFileSystemEntries(dir))
+                {
+                    if (File.Exists(d))
+                    {
+                        output.Add(d);
+                    }
+                    else
+                        ListAllFiles(d, ref output);
+                }
+            }
+        }
+        private void pathToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = files.Count;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = files[i].shortpath_;
+            }
+            if (dumplsit.Count < 1) return;
+            dataGridView2.RowCount = dumplsit.Count;
+            for (int i = 0; i < dumplsit.Count; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = files[dumplsit[i]].shortpath_;
+            }
+        }
+        private void nameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = files.Count;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = files[i].name_;
+            }
+            if (dumplsit.Count < 1) return;
+            dataGridView2.RowCount = dumplsit.Count;
+            for (int i = 0; i < dumplsit.Count; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = files[dumplsit[i]].name_;
+            }
+        }
+        private void sizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = files.Count;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = files[i].size_;
+            }
+            if (dumplsit.Count < 1) return;
+            dataGridView2.RowCount = dumplsit.Count;
+            for (int i = 0; i < dumplsit.Count; i++)
+            {
+                dataGridView2.Rows[i].Cells[0].Value = files[dumplsit[i]].size_;
+            }
+        }     
+        private void moveToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (dumplsit.Count < 1) return;
+            string path = OpenFolder();
+            if (path == null || path == "") { return; }
+            for(int i = 0; i < dumplsit.Count; i++)
+            {
+                try {
+                    File.Move(files[dumplsit[i]].path_, path + files[dumplsit[i]].shortpath_);
+                }
+                catch { }
+            }
+        }
+        private void dataGridView2_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {for(int j=0;j< dataGridView1.Rows.Count; j++) { 
+            dataGridView1.Rows[j].Selected = false;
+            }
+            int i = dumplsit[e.RowIndex];
+            dataGridView1.Rows[i].Selected = true;
+        }
+    }
+}
