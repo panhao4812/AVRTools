e9a15111a8283c647c1557fab457cdd172427d45
panhao <29347213@qq.com>
2021-07-05 22:38:52 +0800

    update

--- AVRTools/AVRKeys.Designer.cs
+++ AVRTools/AVRKeys.Designer.cs
@@ -49,6 +49,17 @@
             this.Layer1 = new System.Windows.Forms.TabPage();
             this.Layer2 = new System.Windows.Forms.TabPage();
             this.Schematic = new System.Windows.Forms.TabPage();
+            this.WS2812 = new System.Windows.Forms.TabPage();
+            this.BackRGB = new System.Windows.Forms.TabPage();
+            this.iSOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iSO60ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SelectKeysPanel.SuspendLayout();
             this.MatrixPanel.SuspendLayout();
@@ -56,6 +67,7 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Font = new System.Drawing.Font("Courier New", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
@@ -66,7 +78,8 @@
             this.toolsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1135, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(834, 25);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -77,31 +90,33 @@
             this.saveToolStripMenuItem,
             this.saveAsToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(52, 21);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(140, 26);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(140, 26);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(140, 26);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             // 
             // matrixToolStripMenuItem
             // 
+            this.matrixToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iSOToolStripMenuItem});
             this.matrixToolStripMenuItem.Name = "matrixToolStripMenuItem";
-            this.matrixToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.matrixToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
             this.matrixToolStripMenuItem.Text = "Matrix";
             // 
             // wS2812ToolStripMenuItem
@@ -110,31 +125,31 @@
             this.defaultONToolStripMenuItem,
             this.fixColorsToolStripMenuItem});
             this.wS2812ToolStripMenuItem.Name = "wS2812ToolStripMenuItem";
-            this.wS2812ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.wS2812ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
             this.wS2812ToolStripMenuItem.Text = "WS2812";
             // 
             // defaultONToolStripMenuItem
             // 
             this.defaultONToolStripMenuItem.Name = "defaultONToolStripMenuItem";
-            this.defaultONToolStripMenuItem.Size = new System.Drawing.Size(164, 26);
+            this.defaultONToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.defaultONToolStripMenuItem.Text = "Default ON";
             // 
             // fixColorsToolStripMenuItem
             // 
             this.fixColorsToolStripMenuItem.Name = "fixColorsToolStripMenuItem";
-            this.fixColorsToolStripMenuItem.Size = new System.Drawing.Size(164, 26);
+            this.fixColorsToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.fixColorsToolStripMenuItem.Text = "Fix Colors";
             // 
             // uploadToolStripMenuItem
             // 
             this.uploadToolStripMenuItem.Name = "uploadToolStripMenuItem";
-            this.uploadToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.uploadToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
             this.uploadToolStripMenuItem.Text = "Upload";
             // 
             // testKeyToolStripMenuItem
             // 
             this.testKeyToolStripMenuItem.Name = "testKeyToolStripMenuItem";
-            this.testKeyToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.testKeyToolStripMenuItem.Size = new System.Drawing.Size(76, 21);
             this.testKeyToolStripMenuItem.Text = "TestKey";
             // 
             // toolsToolStripMenuItem
@@ -142,41 +157,45 @@
             this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.hidRawToolStripMenuItem});
             this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
-            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(60, 21);
             this.toolsToolStripMenuItem.Text = "Tools";
             // 
             // hidRawToolStripMenuItem
             // 
             this.hidRawToolStripMenuItem.Name = "hidRawToolStripMenuItem";
-            this.hidRawToolStripMenuItem.Size = new System.Drawing.Size(140, 26);
+            this.hidRawToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.hidRawToolStripMenuItem.Text = "HidRaw";
             // 
             // SelectKeysPanel
             // 
             this.SelectKeysPanel.Controls.Add(this.US);
             this.SelectKeysPanel.Controls.Add(this.Macro);
-            this.SelectKeysPanel.Location = new System.Drawing.Point(4, 370);
+            this.SelectKeysPanel.Font = new System.Drawing.Font("Courier New", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectKeysPanel.Location = new System.Drawing.Point(0, 273);
+            this.SelectKeysPanel.Margin = new System.Windows.Forms.Padding(2);
             this.SelectKeysPanel.Name = "SelectKeysPanel";
             this.SelectKeysPanel.SelectedIndex = 0;
-            this.SelectKeysPanel.Size = new System.Drawing.Size(1127, 290);
+            this.SelectKeysPanel.Size = new System.Drawing.Size(838, 250);
             this.SelectKeysPanel.TabIndex = 5;
             // 
             // US
             // 
-            this.US.Location = new System.Drawing.Point(4, 25);
+            this.US.Location = new System.Drawing.Point(4, 26);
+            this.US.Margin = new System.Windows.Forms.Padding(2);
             this.US.Name = "US";
-            this.US.Padding = new System.Windows.Forms.Padding(3);
-            this.US.Size = new System.Drawing.Size(1119, 261);
+            this.US.Padding = new System.Windows.Forms.Padding(2);
+            this.US.Size = new System.Drawing.Size(830, 220);
             this.US.TabIndex = 0;
             this.US.Text = "US";
             this.US.UseVisualStyleBackColor = true;
             // 
             // Macro
             // 
-            this.Macro.Location = new System.Drawing.Point(4, 25);
+            this.Macro.Location = new System.Drawing.Point(4, 26);
+            this.Macro.Margin = new System.Windows.Forms.Padding(2);
             this.Macro.Name = "Macro";
-            this.Macro.Padding = new System.Windows.Forms.Padding(3);
-            this.Macro.Size = new System.Drawing.Size(1123, 261);
+            this.Macro.Padding = new System.Windows.Forms.Padding(2);
+            this.Macro.Size = new System.Drawing.Size(830, 220);
             this.Macro.TabIndex = 1;
             this.Macro.Text = "Macro";
             this.Macro.UseVisualStyleBackColor = true;
@@ -186,53 +205,147 @@
             this.MatrixPanel.Controls.Add(this.Layer1);
             this.MatrixPanel.Controls.Add(this.Layer2);
             this.MatrixPanel.Controls.Add(this.Schematic);
-            this.MatrixPanel.Location = new System.Drawing.Point(0, 31);
+            this.MatrixPanel.Controls.Add(this.WS2812);
+            this.MatrixPanel.Controls.Add(this.BackRGB);
+            this.MatrixPanel.Font = new System.Drawing.Font("Courier New", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MatrixPanel.Location = new System.Drawing.Point(0, 25);
+            this.MatrixPanel.Margin = new System.Windows.Forms.Padding(2);
             this.MatrixPanel.Name = "MatrixPanel";
             this.MatrixPanel.SelectedIndex = 0;
-            this.MatrixPanel.Size = new System.Drawing.Size(1131, 333);
+            this.MatrixPanel.Size = new System.Drawing.Size(838, 250);
             this.MatrixPanel.TabIndex = 6;
             // 
             // Layer1
             // 
-            this.Layer1.Location = new System.Drawing.Point(4, 25);
+            this.Layer1.Location = new System.Drawing.Point(4, 26);
+            this.Layer1.Margin = new System.Windows.Forms.Padding(2);
             this.Layer1.Name = "Layer1";
-            this.Layer1.Padding = new System.Windows.Forms.Padding(3);
-            this.Layer1.Size = new System.Drawing.Size(1123, 304);
+            this.Layer1.Padding = new System.Windows.Forms.Padding(2);
+            this.Layer1.Size = new System.Drawing.Size(830, 220);
             this.Layer1.TabIndex = 0;
             this.Layer1.Text = "Layer1";
             this.Layer1.UseVisualStyleBackColor = true;
             // 
             // Layer2
             // 
-            this.Layer2.Location = new System.Drawing.Point(4, 25);
+            this.Layer2.Location = new System.Drawing.Point(4, 26);
+            this.Layer2.Margin = new System.Windows.Forms.Padding(2);
             this.Layer2.Name = "Layer2";
-            this.Layer2.Padding = new System.Windows.Forms.Padding(3);
-            this.Layer2.Size = new System.Drawing.Size(1123, 304);
+            this.Layer2.Padding = new System.Windows.Forms.Padding(2);
+            this.Layer2.Size = new System.Drawing.Size(830, 220);
             this.Layer2.TabIndex = 1;
             this.Layer2.Text = "Layer2";
             this.Layer2.UseVisualStyleBackColor = true;
             // 
             // Schematic
             // 
-            this.Schematic.Location = new System.Drawing.Point(4, 25);
+            this.Schematic.Location = new System.Drawing.Point(4, 26);
+            this.Schematic.Margin = new System.Windows.Forms.Padding(2);
             this.Schematic.Name = "Schematic";
-            this.Schematic.Padding = new System.Windows.Forms.Padding(3);
-            this.Schematic.Size = new System.Drawing.Size(1123, 304);
+            this.Schematic.Padding = new System.Windows.Forms.Padding(2);
+            this.Schematic.Size = new System.Drawing.Size(830, 220);
             this.Schematic.TabIndex = 2;
             this.Schematic.Text = "Schematic";
             this.Schematic.UseVisualStyleBackColor = true;
             // 
+            // WS2812
+            // 
+            this.WS2812.Location = new System.Drawing.Point(4, 26);
+            this.WS2812.Name = "WS2812";
+            this.WS2812.Padding = new System.Windows.Forms.Padding(3);
+            this.WS2812.Size = new System.Drawing.Size(830, 220);
+            this.WS2812.TabIndex = 3;
+            this.WS2812.Text = "WS2812";
+            this.WS2812.UseVisualStyleBackColor = true;
+            // 
+            // BackRGB
+            // 
+            this.BackRGB.Location = new System.Drawing.Point(4, 26);
+            this.BackRGB.Name = "BackRGB";
+            this.BackRGB.Padding = new System.Windows.Forms.Padding(3);
+            this.BackRGB.Size = new System.Drawing.Size(830, 220);
+            this.BackRGB.TabIndex = 4;
+            this.BackRGB.Text = "BackRGB";
+            this.BackRGB.UseVisualStyleBackColor = true;
+            // 
+            // iSOToolStripMenuItem
+            // 
+            this.iSOToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iSO60ToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8});
+            this.iSOToolStripMenuItem.Name = "iSOToolStripMenuItem";
+            this.iSOToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.iSOToolStripMenuItem.Text = "ISO";
+            // 
+            // iSO60ToolStripMenuItem
+            // 
+            this.iSO60ToolStripMenuItem.Name = "iSO60ToolStripMenuItem";
+            this.iSO60ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.iSO60ToolStripMenuItem.Text = "61";
+            this.iSO60ToolStripMenuItem.Click += new System.EventHandler(this.ISO60_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem2.Text = "63";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.ISO61_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem3.Text = "64";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.ISO64_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem4.Text = "68";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem5.Text = "84";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem6.Text = "100";
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem7.Text = "104";
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem8.Text = "108";
+            // 
             // AVRKeys
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1135, 661);
+            this.ClientSize = new System.Drawing.Size(834, 523);
             this.Controls.Add(this.MatrixPanel);
             this.Controls.Add(this.SelectKeysPanel);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AVRKeys";
             this.Text = "AVRKeys";
             this.Load += new System.EventHandler(this.AVRKeys_Load);
@@ -266,6 +379,17 @@
         private System.Windows.Forms.TabPage Schematic;
         private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hidRawToolStripMenuItem;
+        private System.Windows.Forms.TabPage WS2812;
+        private System.Windows.Forms.TabPage BackRGB;
+        private System.Windows.Forms.ToolStripMenuItem iSOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iSO60ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
     }
 }
 
--- AVRTools/AVRKeys.cs
+++ AVRTools/AVRKeys.cs
@@ -10,17 +10,80 @@ using System.Windows.Forms;
 
 namespace AVRTools
 {
-    public partial class AVRKeys : Form
-    {
-        /*
+    /*
          * 原型
          * */
+    public partial class AVRKeys : Form
+    {
+        public void DefaultLayout()
+        {
+            this.Size = new Size(850, 561);
+            MatrixPanel.Size = new Size(838, 250);
+            MatrixPanel.Location = new Point(0, 25);
+            SelectKeysPanel.Size = new Size(838, 250);
+            SelectKeysPanel.Location = new Point(0, 272);
+        }
+        
         public AVRKeys()
         {
             InitializeComponent();
         }
 
+        private void AddButton(int index, string str)
+        {
+            /*
+            TempImage = KeymapPanel.BackgroundImage;
+            KeymapPanel.BackgroundImage = null;
+            double x = ActiveMatrix.keycap[index, 0];
+            double y = ActiveMatrix.keycap[index, 1];
+            double length = ActiveMatrix.keycap[index, 2];
+            double row = ActiveMatrix.keycap[index, 3];
+            double col = ActiveMatrix.keycap[index, 4];
+            Button button = new Button();
+            KeymapPanel.Controls.Add(button);
+            Size size1 = new Size((int)(KeycapLength * length - KeycapOffset * 2), (int)(KeycapLength - KeycapOffset * 2));
+            Point Point1 = new Point(40 + (int)(x * KeycapLength), 100 + (int)(y * KeycapLength));
+            if (x > 14)
+            {
+                Point1.X += 12;
+            }
+            if (y < 0)
+            {
+                Point1.Y -= 9;
+            }
+            if (length == 0.5)
+            {
+                size1.Width = (int)(KeycapLength - KeycapOffset * 2);
+                size1.Height = (int)(KeycapLength * 2 - KeycapOffset * 2);
+            }
+            button.Size = size1;
+            button.Location = Point1;
+            button.FlatStyle = FlatStyle.Flat;
+            button.BackColor = Color.White;
+            button.MouseDown += new MouseEventHandler(Layer0Button_MouseClick);
+            button.Text = str;
+            button.Name = index.ToString();
+            // button.TabStop = false; //禁用tab键    
+            KeymapPanel.BackgroundImage = TempImage;
+            */
+        }
+
         private void AVRKeys_Load(object sender, EventArgs e)
+        {
+            DefaultLayout();
+        }
+
+        private void ISO60_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ISO61_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ISO64_Click(object sender, EventArgs e)
         {
 
         }
